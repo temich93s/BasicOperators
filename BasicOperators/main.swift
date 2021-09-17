f6927630a115c4088b8b1bb957990d24714b30b6
Temich93s <nil>
2021-09-18 09:34:14 +1000

    Переделал раздел: Операторы сравнения (сделал вывод результата через интерполяцию строки)

--- BasicOperators/main.swift
+++ BasicOperators/main.swift
@@ -7,8 +7,8 @@
 
 import Foundation
 
-print("Hello, World!")
 
+print("Hello, World!")
 // MARK: Оператор присваивания
 
 print("\n//Арифмитические операторы")
@@ -20,8 +20,8 @@ print(a)
 let (x,y) = (1, 2) //x равен 1, а y равен 2
 print(x,y)
 
-// MARK: Арифметические операторы
 
+// MARK: Арифметические операторы
 print("\n//Арифмитические операторы")
 var a1 = 1 + 2 // равно 3
 var a2 = 5 - 3 // равно 2
@@ -32,20 +32,21 @@ print(a1, a2, a3, a4)
 var a5 = "hel" + "lo" //равно "hello"
 print(a5)
 
-// MARK: Оператор целочисленного деления
 
+// MARK: Оператор целочисленного деления
 print("\n//Оператор целочисленного деления")
 let b1 = 9 % 4 // равно 1
 let b2 = -9 % 4 // равно -1
 print(b1, b2)
 
-// MARK: Оператор унарного минуса
 
+// MARK: Оператор унарного минуса
 print("\n//Оператор унарного минуса")
 let three = 3
 let minusThree = -three // minusThree равно -3
 let plusThree = -minusThree // plusThree равно 3 (так как "минус минус три")
 print(three, minusThree, plusThree)
+
 
 // MARK: Составные операторы присваивания
 print("\n//Составные операторы присваивания")
@@ -53,14 +54,23 @@ var c1 = 1
 c1 += 2 // теперь с1 равно 3
 print(c1)
 
+
 // MARK: Операторы сравнения
 print("\n//Операторы сравнения")
+/*
 1 == 1 ? print(true) : print(false) // истина, так как 1 равно 1
 2 != 1 ? print(true) : print(false) // истина, так как 2 не равно 1
 2 > 1 ? print(true) : print(false) // истина, так как 2 больше чем 1
 1 < 2 ? print(true) : print(false) // истина, так как 1 меньше 2
 1 >= 1 ? print(true) : print(false) // истина, так как 1 больше либо равно 1
 2 <= 1 ? print(true) : print(false) // ложь, так как 2 не меньше либо равно 1
+*/
+print("Результат сравнения 2 != 1: \(1 == 1)")
+print("Результат сравнения 2 != 1: \(2 != 1)")
+print("Результат сравнения 2 != 1: \(2 > 1)")
+print("Результат сравнения 2 != 1: \(1 < 2)")
+print("Результат сравнения 2 != 1: \(1 >= 1)")
+print("Результат сравнения 2 != 1: \(2 <= 1)")
 
 let name = "world"
 if name == "world" {
@@ -76,10 +86,16 @@ if name == "world" {
 ("blue", -1) < ("blue", 1) ? print(true) : print(false) // true
 //("blue", false) <= ("blue", true) ? print(true) : print(false) // ошибка - операторы <, >, <=, >= не могут применяться к bool
 
+
 // MARK: Тернарный условный оператор
 print("\n//Тернарный условный оператор")
 let contentHeight = 40
 let hasHeader = true
 print("\(contentHeight + (hasHeader ? 50 : 20))") // 90
 print("\(contentHeight + (!hasHeader ? 50 : 20))") // 60
+
+
+// MARK: Оператор объединения по nil
+print("\n//Оператор объединения по nil")
+
 
